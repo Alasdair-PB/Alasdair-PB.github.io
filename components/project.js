94ae0384adf0ff18a2ebea3c7a0b9cb5bfa26d89
Alasdair Pilmore <a.pilmore-bedford@unity3d.com>
2024-01-24 11:29:35 +0000

    styling updates

--- components/project.js
+++ components/project.js
@@ -24,6 +24,7 @@ class Project extends Json {
     
       processData(jsonData) {
         let htmlContent = `
+
         <div class="card">
             <h2>${jsonData.name}</h2>
             <p>${jsonData.text}</p>
