88e05d420f2abfad1a4f70525bab3ddbae6c20e8
Alasdair Pilmore <a.pilmore-bedford@unity3d.com>
2024-01-24 23:54:22 +0000

    Js script setup

---
+++ components/profile.js
@@ -0,0 +1,22 @@
+/*<div class="profile">
+    <div class="intro">
+        <img src="misc/Img/art/me.jfif">
+        <h1 class="title">Project Title</h1>
+
+        <h2>About this project</h2>
+        <p>
+            A project designed to x
+        </p>
+
+        <p>
+            This project was 
+        </p>
+    </div>
+</div>*/
+
+class Profile extends Json {
+
+
+}
+
+customElements.define('profile-component', Profile);
